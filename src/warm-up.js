0faba3025c88e1770944ffda7e3073a67ee78453
Andrew Miller <andrew.r.miller45@gmail.com>
2022-03-24 16:25:48 -0700

    warm up revision

--- src/warm-up.js
+++ src/warm-up.js
@@ -25,21 +25,20 @@ console.log(inStock);
 // Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack!
 // Use a built in math operator to decrement the inStock count by 1.
 inStock = inStock - 1;
-
+console.log(inStock);
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
 var isFavorite = false;
-
 
 // Create a new variable named "shouldOrderMore", and use a comparison operator to
 // assign a boolean value to this variable. If "inStock" is less than 20, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 var shouldOrderMore
-if (inStock < 20) {
-  shouldOrderMore = true
+  if (inStock < 20) {
+    console.log();(`We need to order more!`)
 } else {
-  shouldOrderMore = false
+    console.log(`We don't need to order.`);
 }
 console.log(shouldOrderMore);
 
